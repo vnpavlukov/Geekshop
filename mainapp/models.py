aa04af4fb563d70e70d385220b35163a86070cb7
Vladimir <alfa.voland@gmail.com>
2021-03-04 17:39:09 +0300

    get names for categories

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=40, unique=True)
     description = models.TextField(blank=True)  # blank=True field can be empty
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -16,3 +19,6 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     # CASCADE если удалить категорию, то удалятся и все продукты в ней
     # PROTECT нельзя удалить категорию если есть хоть один продукт
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'''
