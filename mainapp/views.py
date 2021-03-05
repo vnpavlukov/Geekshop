577b0a59a7e094e20ef003b83126e81ae3708eec
Vladimir <alfa.voland@gmail.com>
2021-03-05 13:11:41 +0300

    delete json file

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,8 +5,6 @@ import datetime
 
 from mainapp.models import ProductCategory, Product
 
-products_file_name = os.path.join(os.path.dirname(__file__), 'fixtures/data_products.json')
-
 
 # функции - вьюхи - контроллеры
 def index(request):
@@ -27,24 +25,3 @@ def products(request):
                'categories': ProductCategory.objects.all()
                }
     return render(request, 'mainapp/products.html', context)
-
-
-def test_context(request):
-    context = {
-        'title': 'geekshop',
-        'header': 'Добро пожаловать на сайт',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами', 'price': '6 600,00'},
-            {'name': 'Синяя куртка', 'price': '23 550,00'},
-            {'name': 'Коричневый спортивный топ', 'price': '7 500,00'},
-        ],
-        'datetime': datetime.datetime.now(),
-        'promotion': False,
-        'product_promotion': [
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00'},
-        ]
-    }
-
-    return render(request, 'mainapp/test-context.html', context)
