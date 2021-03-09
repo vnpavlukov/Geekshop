4000645526a3819afc6d93de317d745c4c8e72da
Vladimir <alfa.voland@gmail.com>
2021-03-09 19:23:01 +0300

    added logout

--- authapp/views.py
+++ authapp/views.py
@@ -30,8 +30,15 @@ def register(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('auth:login'))
+        else:
+            print(form.errors)
     else:
         form = UserRegisterForm()
     context = {'form': form}
 
     return render(request, 'authapp/register.html', context)
+
+
+def logout(request):
+   auth.logout(request)
+   return HttpResponseRedirect(reverse('index'))
