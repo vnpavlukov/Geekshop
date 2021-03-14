97cba0da1fcdde7278a44f3b2597c5d687ef6fa5
Vladimir <alfa.voland@gmail.com>
2021-03-14 19:28:37 +0300

    clear code

--- basketapp/models.py
+++ basketapp/models.py
@@ -8,7 +8,7 @@ class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
-    create_timestamp = models.DateTimeField(auto_now_add=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Корзина дял {self.user.name} | Продукт  {self.product.name}'
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
