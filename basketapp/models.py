5d0b00bc9630d4bacc37215d537f14aec29bc09b
Vladimir <alfa.voland@gmail.com>
2021-03-14 08:20:17 +0300

    basket from profile.html into basket.html

--- basketapp/models.py
+++ basketapp/models.py
@@ -4,7 +4,7 @@ from authapp.models import User
 from mainapp.models import Product
 
 
-class Bsket(models.Model):
+class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
