8e9db0d1d277f4b029c06cc258598dad02516e0b
Vladimir <alfa.voland@gmail.com>
2021-04-26 12:41:46 +0300

    test_site

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,3 +8,16 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'geekshop',
+        'header': 'Welcome on the site',
+        'username': 'Vladimir Pavlukov',
+        'products': [
+            {'name': 'худи', 'price': '3 050'},
+            {'name': 'ботинки', 'price': '4 080'}
+        ]
+    }
+    return render(request, 'mainapp/test-context.html', context)
