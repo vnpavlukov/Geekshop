38d7ba1af932387625a8d6a0fdf4eb1774e6b0e9
Vladimir <alfa.voland@gmail.com>
2021-04-26 19:44:38 +0300

    dynamic operation of urls

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,7 +20,7 @@ from mainapp.views import index, products, test_context
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('products/', products),
+    path('', index, name='index'),
+    path('products/', products, name='products'),
     path('test-context/', test_context),
 ]
